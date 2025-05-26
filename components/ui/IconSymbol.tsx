333d9b6aa8e1a82b0b8f00319e659c6dbbb0f806
Valent Nathanael <77603243+VennethN@users.noreply.github.com>
2025-05-26 14:48:11 +0700

    Revert "clean stuff"
    This reverts commit 24ab4f18800ad5e73ea202df8566b10e8e0769d2.

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -1,9 +1,14 @@
+// This file is a fallback for using MaterialIcons on Android and web.
+
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { SymbolWeight } from "expo-symbols";
 import React from "react";
-import { OpaqueColorValue, StyleProp, TextStyle } from "react-native";
+import { OpaqueColorValue, StyleProp, ViewStyle } from "react-native";
 
+// Add your SFSymbol to MaterialIcons mappings here.
 const MAPPING = {
+  // See MaterialIcons here: https://icons.expo.fyi
+  // See SF Symbols in the SF Symbols app on Mac.
   "house.fill": "home",
   "paperplane.fill": "send",
   "chevron.left.forwardslash.chevron.right": "code",
@@ -17,6 +22,11 @@ const MAPPING = {
 
 export type IconSymbolName = keyof typeof MAPPING;
 
+/**
+ * An icon component that uses native SFSymbols on iOS, and MaterialIcons on Android and web. This ensures a consistent look across platforms, and optimal resource usage.
+ *
+ * Icon `name`s are based on SFSymbols and require manual mapping to MaterialIcons.
+ */
 export function IconSymbol({
   name,
   size = 24,
@@ -26,7 +36,7 @@ export function IconSymbol({
   name: IconSymbolName;
   size?: number;
   color: string | OpaqueColorValue;
-  style?: StyleProp<TextStyle>;
+  style?: StyleProp<ViewStyle>;
   weight?: SymbolWeight;
 }) {
   return (
